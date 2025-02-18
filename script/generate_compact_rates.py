dd1c7a61135e84ec30d12f54b29e30c357e8dc52
oddaf <106770775+oddaf@users.noreply.github.com>
2025-02-17 22:57:45 -0300

    chore: license agreement on the contract generator script

--- script/generate_compact_rates.py
+++ script/generate_compact_rates.py
@@ -63,7 +63,21 @@ def generate_contract() -> str:
     compact_bytes = f'hex"{"".join(all_bytes)}"'
     
     # Create the contract
-    contract_template = f'''// SPDX-License-Identifier: MIT
+    contract_template = f'''// SPDX-FileCopyrightText: 2025 Dai Foundation <www.daifoundation.org>
+// SPDX-License-Identifier: AGPL-3.0-or-later
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
 pragma solidity ^0.8.24;
 
 contract Conv {{
